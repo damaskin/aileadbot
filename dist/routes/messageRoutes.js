5215ef887bbd10b65b7e70df04155d0884fa4f61
Ivan Damaskin <dmaskin@live.ru>
2024-07-30 15:37:06 +0700

    app init

--- dist/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const messageController_1 = require("../controllers/messageController");
-const router = (0, express_1.Router)();
-router.post('/', messageController_1.sendMessageAvito);
-router.post('/:message_id', messageController_1.deleteMessageAvito);
-exports.default = router;
